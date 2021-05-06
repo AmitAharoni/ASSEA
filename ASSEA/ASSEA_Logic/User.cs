f2b2a132427648dd3624f17f77486b3feca1f05c
AmitAharoni <aharoni94@gmail.com>
2021-05-06 18:51:59 +0300

    merge the User

--- ASSEA/ASSEA_Logic/User.cs
+++ ASSEA/ASSEA_Logic/User.cs
@@ -24,21 +24,29 @@ namespace ASSEA_Logic
           int physicalScale = 50, mentalScale = 50; //need to be between 0 - 100
           int notificationAnsweredByUser = 0;
 
-          List<string> physicalMsg = new List<string>{"Snack time", "Drink Somthing", "Coffee Time",};
-          List<string> mentalMsg = new List<string> { "Fix your posture", "Do some streches", "Wash your face"};
-          
+          List<string> physicalMsgs = new List<string> { "Snack time", "Drink Somthing", "Coffee Time", };
+          List<string> mentalMsgs = new List<string> { "Fix your posture", "Do some streches", "Wash your face" };
+
+
           public enum eInterset
           {
                Sport = 0,
                Music = 1,
                News = 2
           }
-          
+
           public enum eNotificationsLevel
           {
                soft = 0,
                normal = 1,
                extreme = 2
+          }
+
+          public enum eQuery
+          {
+               phy = 0,
+               mental = 1,
+               breakOrWork = 2
           }
 
           public int selectListMSG()
@@ -54,32 +62,52 @@ namespace ASSEA_Logic
           public void pickMessage(int listIndecator)
           {
                Random rd = new Random();
+               eQuery msgType;
                int maxlength = 0;
                string message;
-               if(listIndecator == phy)
-               {
-                    maxlength = physicalMsg.Capacity - 1;
-               }
-               else
-               {
-                    maxlength = mentalMsg.Capacity - 1;
-               }
+               maxlength = listIndecator == phy ? (physicalMsgs.Capacity - 1) : (mentalMsgs.Capacity - 1);
 
                int rand_num = rd.Next(0, maxlength);
 
                if(listIndecator == phy)
                {
-                    message = physicalMsg[rand_num];
+                    message = physicalMsgs[rand_num];
+                    msgType = eQuery.phy;
                }
                else
                {
-                    message = mentalMsg[rand_num];
+                    message = mentalMsgs[rand_num];
+                    msgType = eQuery.mental;
+
                }
 
-               //send notification
+               doWhenMSGready(message, msgType);
           }
 
-          public void pickMessage(int listIndecator)
+          private event Action<string, eQuery> msgNotifier;
+
+          private void doWhenMSGready(string msgToPass, eQuery msgType)
           {
+               msgNotifier.Invoke(msgToPass, msgType);
           }
+
+          public void afterMsgAction(eQuery msgType, bool answer)
+          {
+               if(msgType == eQuery.mental)
+               {
+                    mentalScale = (answer == true) ? mentalScale += 20 : mentalScale -= 20; 
+               }
+               else if(msgType == eQuery.phy)
+               {
+                    physicalScale = (answer == true) ? physicalScale += 20 : physicalScale -= 20;
+
+               }
+               else
+               {
+                    mentalScale = (answer == true) ? mentalScale += 20 : mentalScale -= 20;
+                    physicalScale = (answer == true) ? physicalScale += 20 : physicalScale -= 20;
+               }
+          }
+
+     }
 }
